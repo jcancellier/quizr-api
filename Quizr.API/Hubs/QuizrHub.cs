75d6e3b69d32ae7b3e23f4318d2e1ec2e8d28d4f
Joshua Cancellier <jcancellier@csub.edu>
2019-10-23 01:50:04 -0700

    make it better

--- Quizr.API/Hubs/QuizrHub.cs
+++ Quizr.API/Hubs/QuizrHub.cs
@@ -13,12 +13,45 @@ namespace Quizr.API.Hubs
         /// <summary>
         /// Dictionary mapping connectionIds to Users
         /// </summary>
-        private static ConcurrentDictionary<string, User> QuizrClients = new ConcurrentDictionary<string, User>();
+        private static ConcurrentDictionary<string, User> quizrClients = new ConcurrentDictionary<string, User>();
 
         public override Task OnConnectedAsync()
         {
             Console.WriteLine($"{Context.ConnectionId} connected");
             return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            User userToDelete = quizrClients.Where(client => client.Value.ConnectionId == Context.ConnectionId).FirstOrDefault().Value;
+            bool userRemoved;
+
+            if(userToDelete != null)
+            {
+                userRemoved = quizrClients.TryRemove(userToDelete.Name, out _);
+                if (userRemoved)
+                    Console.WriteLine("user removed");
+                else
+                    Console.WriteLine("Unable to remove user");
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public User Login(string name)
+        {
+            // If new user
+            if (!quizrClients.ContainsKey(name))
+            {
+                User newUser = new User { Name = name, ConnectionId = Context.ConnectionId };
+                bool isAddUserSuccessful = quizrClients.TryAdd(name, newUser);
+                if (!isAddUserSuccessful)
+                    throw new Exception("Unable to add user");
+
+                return newUser;
+            }
+
+            throw new HubException("User already exists");
         }
     }
 }
